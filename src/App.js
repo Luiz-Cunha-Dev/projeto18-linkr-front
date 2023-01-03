8c20905518767d03b800e7ba4e40bea312a7a6de
mcrestani <marianascrestani@gmail.com>
2023-01-03 08:47:08 -0300

    feat: create globalstyle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Globalstyle from "./styles/Globalstyle.js";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Globalstyle />
+    </>
+  );
 }
