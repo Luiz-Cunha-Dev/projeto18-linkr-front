cc1a03f3ddee31ac3b9ed3d93815a66d5c1561ee
mcrestani <marianascrestani@gmail.com>
2023-01-03 17:11:01 -0300

    feat: layout create post

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Globalstyle from "./styles/Globalstyle.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Timeline from "./pages/Timeline.js";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Globalstyle />
-    </>
+      <Routes>
+        <Route path="/timeline" element={<Timeline />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/pages/Timeline.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import CreatePost from "../components/CreatePost.js";
+
+export default function Timeline() {
+  return (
+    <Container>
+      <CreatePost />
+    </Container>
+  );
+}
+
+const Container = styled.div``;
