fb0d30e22c626283bca287947e550a12d5973570
mcrestani <marianascrestani@gmail.com>
2023-01-05 16:11:48 -0300

    feat: createPost

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 import React from "react";
 import CreatePost from "../components/CreatePost.jsx";
-import Header from '../components/header.jsx';
+import Header from "../components/header.jsx";
 
 export default function Timeline() {
   return (
     <Wraper>
-      <Header/>
+      <Header />
       <h1>timeline</h1>
-
+      <CreatePost />
     </Wraper>
   );
 }
