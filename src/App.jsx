16f329c86e41cb0fb76096e0539bc435d5751975
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-01-09 15:50:25 -0300

    fix: deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalStyle from "./global/globalStyle";
 import userContext from "./contexts/userContexts.jsx";
 import PrivatePage from "./pages/privatePage.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import Timeline from "./pages/Timeline.jsx";
+import GlobalStyle from "./global/globalStyle.js";
 
 export default function App() {
   const [user, setUser] = useState({});
