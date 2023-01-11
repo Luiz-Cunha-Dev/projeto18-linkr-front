aefbc0f5063b32b4deecceb7e32d69d77029ae5a
mcrestani <marianascrestani@gmail.com>
2023-01-10 21:06:09 -0300

    fix: correct BASE_URL

--- src/services/linkrAPI.jsx
+++ src/services/linkrAPI.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-//const BASE_URL = "https://api-linkr-0kjk.onrender.com/";
-const BASE_URL = "http://localhost:4000/";
+const BASE_URL = "https://api-linkr-0kjk.onrender.com/";
+//const BASE_URL = "http://localhost:4000/";
 
 function login(body) {
   return axios.post(`${BASE_URL}signin`, body);
@@ -13,7 +13,6 @@ function logout() {
 function signUp(body) {
   return axios.post(`${BASE_URL}signup`, body);
 }
-
 
 function createPost(body, config) {
   return axios.post(`${BASE_URL}timeline`, body, config);
@@ -36,14 +35,7 @@ function deletePost(postid) {
   });
 }
 
-export {
-  login,
-  logout,
-  signUp,
-  createPost,
-  getPosts,
-  deletePost,
-};
+export { login, logout, signUp, createPost, getPosts, deletePost };
 
 /*
 
